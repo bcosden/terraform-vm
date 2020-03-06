7c9539e319594fdf52f980aad7cda29db3fe012d
Bruce M. Cosden <bruce@cosden.me>
2020-03-06 17:33:57 -0500

    touch

--- windowsvm.tf
+++ windowsvm.tf
@@ -1,4 +1,4 @@
-## Configure the Microsoft Azure Provider
+# Configure the Microsoft Azure Provider
 # sp name: "azure-cli-2020-03-06-14-52-57"
 provider "azurerm" {
     version         = "~> 1.44"
