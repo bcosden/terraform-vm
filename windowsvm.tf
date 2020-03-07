7d026aa53909fa9f4f6cf78bab961a3396af12f4
Bruce M. Cosden <bruce@cosden.me>
2020-03-06 19:41:46 -0500

    remove fmt

--- windowsvm.tf
+++ windowsvm.tf
@@ -11,7 +11,7 @@ provider "azurerm" {
 
 # Create a resource group if it doesn’t exist
 resource "azurerm_resource_group" "myterraformgroup" {
-    name     = "myResourceGroup"
+    name     = "azterraformgroup"
     location = "eastus"
 
     tags = {
